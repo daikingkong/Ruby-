472e1f722d50cafd12c7d9bde557d2a747841ef5
EC2 Default User <daikingkong>
2022-07-11 05:55:46 +0000

    [Finish]ruby 6-1, 6-2

---
+++ fukusyuu.rb
@@ -0,0 +1,40 @@
+puts "計算をはじめます"
+puts "何回計算を繰り返しますか？"
+
+count = gets.to_i
+number = 1
+
+while number <= count do
+
+  puts "#{number}回目の計算"
+  puts "2つの値を入力してください"
+
+  a = gets.to_i
+  b = gets.to_i
+
+  puts "a=#{a}"
+  puts "b=#{b}"
+
+  puts "計算結果を出力します"
+
+  puts "a+b=#{a+b}"
+  puts "a-b=#{a-b}"
+  puts "a*b=#{a*b}"
+  puts "a/b=#{a/b}"
+
+  number += 1
+
+end
+
+puts "計算を終了します"
+
+# [ruby] 6-1
+puts "計算をはじめます"
+puts "2つの値を入力してください"
+
+a = gets
+b = gets
+
+puts "計算結果を出力します"
+puts "a*b=#{(a.to_i*b.to_i)}"
+puts "計算を終了します"
